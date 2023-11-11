6c3767aeac5273b0b8bc33d052f76c9a73e89a94
flrd <m4rkus.doering@gmail.com>
2023-11-11 21:13:52 +0100

    make helper a little more robust

--- R/get_daily_sequence.R
+++ R/get_daily_sequence.R
@@ -7,12 +7,41 @@
 #' @seealso [seq.Date()] which this function wraps.
 get_daily_sequence <- function(start_date, end_date) {
 
-  start_date <- as.Date(start_date)
-  end_date <- as.Date(end_date)
+  if (length(start_date) != 1L || length(end_date) != 1L) {
+    stop("'start_date and 'end_date' must be of length one.")
+  }
+
+  start_date <- as_date(start_date)
+  end_date <- as_date(end_date)
+
+  if (anyNA(c(start_date, end_date))) {
+    stop("'start_date and 'end_date' must follow the ISO 8601 date format, i.e. '%Y-%m-%d'.")
+  }
 
   stopifnot(start_date <= end_date)
 
   seq.Date(from = start_date, to = end_date, by = "day")
 }
 
+is_date <- function(x) {
+  inherits(x, "Date")
+}
+as_date <- function(x) {
 
+  if (is_date(x)) {
+    return(x)
+  }
+
+  out <- tryCatch(
+    expr = {
+      as.Date.character(x)
+    },
+    error = function(e) {
+      # return value in case of error
+      return(NA_character_)
+    }
+  )
+
+  return(out)
+
+}
