6fa2f583238c252a72f4e2818c6626d612b57410
flrd <m4rkus.doering@gmail.com>
2023-11-13 23:55:32 +0100

    update script to generate interal data

---
+++ R/get_load_profile.R
@@ -0,0 +1,32 @@
+get_load_profile <- function(
+    profile = c("H0", "G0", "G1", "G2", "G3", "G4", "G5", "G6", "L0", "L1", "L2"),
+    start_date = Sys.Date(),
+    end_date = Sys.Date() + 2) {
+
+  profile <- toupper(profile)
+  profile <- match.arg(arg = profile)
+
+  daily_seq <- get_daily_sequence(start_date, end_date)
+  wkday_period <- get_wkday_period(daily_seq)
+
+  tmp <- load_profiles_lst[[profile]][, wkday_period]
+  #
+  # n_rows <- dim(tmp_values)[[1]]
+  #
+  # date_rng <- rep(tmp_dates[["request_period"]], each = n_rows)
+  # time_rng <- dimnames(tmp_values)[[1]]
+  #
+  #
+  #
+  # out <- data.frame(
+  #   start_time = make_utc(date_rng, c(time_rng[n_rows], time_rng[-n_rows])),
+  #   # end_time = make_utc(date_rng, time_rng),
+  #   values = tmp_values |> as.vector()
+  # )
+  #
+  tmp
+
+}
+
+
+
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,22 +1,21 @@
 # attach packages ---------------------------------------------------------
 library(readxl)
 library(data.table)
-
-# https://stackoverflow.com/questions/31589170/download-unzip-and-load-excel-file-in-r-using-tempfiles-only
+library(httr2)
 
 # load data ---------------------------------------------------------------
-
-path <- "~/Downloads/Profile/Repr„sentative Profile VDEW.xls"
+path <- system.file("extdata", "Repr„sentative Profile VDEW.xls", package = "standardlastprofil")
 sheets <- readxl::excel_sheets(path)
 n_sheets <- length(sheets)
 
-# create empty list of length = n_sheets
-output <- vector(mode = "list", length = n_sheets)
+# creates empty list of length = n_sheets
+profiles_lst <- vector(mode = "list", length = n_sheets)
+names(profiles_lst) <- sheets
 
 # populate list
-for (i in seq_len(n_sheets)) {
-  output[[i]] <- readxl::read_excel(
-    sheet = sheets[i],
+for (sheet in sheets) {
+  profiles_lst[[sheet]] <- readxl::read_excel(
+    sheet = sheet,
     path,
     range = "A3:J99",
     col_types = c(
@@ -33,44 +32,143 @@ for (i in seq_len(n_sheets)) {
     )
   )
 }
-names(output) <- sheets
+
 
 # data clean-up -----------------------------------------------------------
-
-# update names
-days <- c("saturday", "sunday", "workingDay")
+days <- c("saturday", "sunday", "working_day")
 periods <- c("winter", "summer", "transition")
 nms <- c("timestamp", rep(periods, each = 3))
 
-tmp <- lapply(output, function(profile) {
-  setDT(profile)
+profiles_lst <- lapply(profiles_lst, function(profile) {
+  # use data.table, set profile to
+  data.table::setDT(profile)
 
   # rename columns
-  setnames(profile, c(nms[1], paste(days, nms[-1], sep = "_")))
+  data.table::setnames(profile, c(nms[1], paste(days, nms[-1], sep = "_")))
 
   # remove date part from 'timestamp' column
-  profile[, timestamp := format(profile$timestamp, "%H:%M")]
-
+  profile[, timestamp := format(timestamp, "%H:%M")]
 })
 
-VDEW_profiles_long <- lapply(tmp, function(profile) {
 
+# create list of matrices to be used internally ---------------------------
+load_profiles_lst <- lapply(profiles_lst, function(profile) {
+  as.matrix(profile, rownames = "timestamp")
+})
+
+
+# create .csv to be added in inst/extdata ---------------------------------
+load_profiles <- lapply(profiles_lst, function(profile) {
   # reshape from wide to long format
-  profile <- melt(profile, measure.vars = patterns(days), value.name = days)
+  profile <- data.table::melt(profile, measure.vars = patterns(days), value.name = days)
 
-  # name values in column 'type' to appropriate period
-  profile[, periods := `levels<-`(variable, periods)]
-
-  # remove column 'variable'
+  # set values in column 'type' to appropriate period, remove column 'variable'
+  profile[, period := `levels<-`(variable, periods)]
   profile[, variable := NULL]
+  profile
 
-  return(profile)
-})
+}) |>
+  data.table::rbindlist(idcol = "profile") |>
+  data.table::melt(
+    measure.vars = c("saturday", "sunday", "working_day"),
+    variable.name = "weekday",
+    value.name = "watt"
+) |>
+  data.table::setcolorder(c("profile", "period", "weekday", "timestamp", "watt"))
 
-VDEW_profiles_long <- data.table::rbindlist(VDEW_profiles_long, idcol = "profile")
+# save VDEW_profiles_wide to disk as CSV
+fwrite(x = load_profiles,
+       file = system.file("/inst/extdata", "load_profiles.csv", package = "standardlastprofil"))
 
 
-usethis::use_data(load_profiles, overwrite = TRUE)
+# fetch public holidays in Germany from nager.Date API --------------------
+years <- seq.int(1973, 2073)
+federal_holidays_DE <- sapply(years, get_federal_holidays)
 
+names(federal_holidays_DE) <- years
 
+# abbreviations of the 11 profiles included in this package ---------------
+profiles <- c("H0", "G0", "G1", "G2", "G3", "G4", "G5", "G6", "L0", "L1", "L2")
+profiles <- factor(profiles, levels = profiles)
 
+# german description ------------------------------------------------------
+description_DE = c(
+  "Haushalt",
+  "Gewerbe allgemein",
+  "Gewerbe werktags 8-18 Uhr",
+  "Gewerbe mit starkem bis überwiegendem Verbrauch in den Abendstunde",
+  "Gewerbe durchlaufend",
+  "Laden/ Friseur",
+  "Bäckerei mit Backstube",
+  "Wochenendbetrieb",
+  "Landwirtschaftsbetriebe allgemein",
+  "Landwirtschaftsbetriebe mit Milchwirtschaft/ Nebenerwerbs-Tierzucht",
+  "Übrige Landwirtschaftsbetriebe"
+)
+
+comment_DE <- c(
+  "",
+  "Gewogener Mittelwert der Profile G1-G6",
+  "z.B. Büros, Arztpraxen, Werkstätten, Verwaltungseinrichtungen",
+  "z.B. Sportvereine, Fitnessstudios, Abendgaststätten",
+  "z.B. Kühlhäuser, Pumpen, Kläranlagen",
+  "",
+  "",
+  "z.B. Kinos",
+  "Gewogener Mittelwert der Profile L1 und L2",
+  "",
+  ""
+)
+
+# english description -----------------------------------------------------
+description_EN = c(
+  "household",
+  "commerce in general",
+  "commerce working day from 8am - 6pm",
+  "commerce with high to predominant consumption in the evening hours",
+  "commerce continous",
+  "shop / barbershop",
+  "bakery with bakehouse",
+  "weekend business",
+  "agricultural businesses in general",
+  "agricultural businesses with dairy farming / part-time livestock farming",
+  "other agricultural businesses"
+)
+
+comment_EN <- c(
+  "",
+  "weighted average of profiles G1-G6",
+  "e.g. offices, doctor's office, workshop, administrative facilities",
+  "e.g. sports club, fitness studio, evening restaurants",
+  "e.g. cold storage warehouse, pumps, sewage treatment plants",
+  "",
+  "",
+  "e.g. movie theater",
+  "weighted average of profiles L1-L2",
+  "",
+  ""
+)
+
+# create data.frame and store internally ----------------------------------
+profile_description_DE <- data.frame(
+  profile = profiles, # factor
+  description = description_DE,
+  comment = comment_DE
+)
+profile_description_EN <- data.frame(
+  profile = profiles, # factor
+  description = description_EN,
+  comment = comment_EN
+)
+
+# store data internally ---------------------------------------------------
+# see: https://r-pkgs.org/data.html#sec-data-sysdata
+
+usethis::use_data(
+  load_profiles_lst,
+  federal_holidays_DE,
+  profile_description_DE,
+  profile_description_EN,
+  internal = TRUE,
+  overwrite = TRUE
+)
